3f51be2c5db7ff6f8f13df636f8688d848c568f0
song jeongho <kheyohsu@song-ui-MacBook-Pro.local>
2020-06-14 18:33:35 +0900

    2020-06-14

--- src/main/java/com/web/BootWebApplication.java
+++ src/main/java/com/web/BootWebApplication.java
@@ -1,29 +1,41 @@
 package com.web;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.stream.IntStream;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.web.domain.Board;
 import com.web.domain.User;
 import com.web.domain.enums.BoardType;
 import com.web.repository.BoardRepository;
 import com.web.repository.UserRepository;
+import com.web.resolver.UserArgumentResolver;
 
 @SpringBootApplication
-public class BootWebApplication {
+public class BootWebApplication implements WebMvcConfigurer {
+	
+	@Autowired
+	private UserArgumentResolver userArgumentResolver;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BootWebApplication.class, args);
 	}
 	
+	@Override
+	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+		argumentResolvers.add(userArgumentResolver);
+	}
+	
 	@Bean
-	public CommandLineRunner runner(UserRepository userRepository, BoardRepository boardRepository) 
-			throws Exception {
+	public CommandLineRunner runner(UserRepository userRepository, BoardRepository boardRepository) {
 		return (args) -> {
 			User user = userRepository.save(User.builder()
 					.name("havi")
---
+++ src/main/java/com/web/controller/LoginController.java
@@ -0,0 +1,21 @@
+package com.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.web.annotation.SocialUser;
+import com.web.domain.User;
+
+@Controller
+public class LoginController {
+
+	@GetMapping("/login")
+	public String login() {
+		return "login";
+	}
+	
+	@GetMapping(value = "/loginSuccess")
+	public String loginComplete(@SocialUser User user) {
+		return "redirect:/board/list";
+	}
+}
